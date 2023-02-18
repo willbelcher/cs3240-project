af0f46eddd77f4965f0a2f6fbbfa15bd9e0abd66
WBelchman <eaglebelcher@me.com>
2023-02-18 15:15:38 -0500

    initial api function

--- schedule/api.py
+++ schedule/api.py
@@ -1,10 +1,22 @@
 import requests
-import json
 
 base_url = "https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearch?institution=UVA01"
+field_pattern = "&{}={}"
 
-def courses_by_dept(year, term, dept_code):
-    pass
+def get_course_list(year=2023, term="Fall", dept=False, instructor=False):
+    num_term = 0
+    if term == "Fall":
+        num_term = 8
+    elif term == "Spring":
+        num_term = 12
 
-def courses_by_instructor(year, term, instructor_name):
-    pass
+    search_url = base_url
+    search_url += field_pattern.format("term", "1{}{}".format(year%100, num_term))
+
+    if dept:
+        search_url += field_pattern.format("subject", dept)
+    if instructor:
+        search_url += field_pattern.format("instructor_name", instructor)
+    
+    resp = requests.get(search_url)
+    courses = resp.json()
