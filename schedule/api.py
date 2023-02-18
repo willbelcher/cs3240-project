19897426781ea4bcfaaede1eed8277500581579f
WBelchman <eaglebelcher@me.com>
2023-02-18 12:35:29 -0500

    method headers

---
+++ schedule/api.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+base_url = "https://sisuva.admin.virginia.edu/psc/ihprd/UVSS/SA/s/WEBLIB_HCX_CM.H_CLASS_SEARCH.FieldFormula.IScript_ClassSearch?institution=UVA01"
+
+def courses_by_dept(year, term, dept_code):
+    pass
+
+def courses_by_instructor(year, term, instructor_name):
+    pass
