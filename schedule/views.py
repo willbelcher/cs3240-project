e820f91fb9c8751e2af5ca8edd8b60eb4e767fa6
glendonchin <glendonchin@gmail.com>
2023-02-27 17:38:07 -0500

    Add user authentication and documentation

--- schedule/views.py
+++ schedule/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
-def post_list(request):
-    user = request.user
-    role = ""
-    username = user.username
+
+@login_required
+def home(request):
+    #Checks to make sure that the user is logged in to assign the correct name
+    if request.user.is_authenticated:
+        user = request.user
+        role = ""
+        username = user.username
+    #Not Logged In is a placeholder currently
+    else:
+        user = "Not Logged In"
+    
+    #Assigns role (either Student or Advisor) based off of the user permissions
     if user.has_perm('global_permissions.is_advisor'):
         role = "Advisor"
     else:
         role = "Student"
-    return render(request, 'schedule/post_list.html', {'role': role, 'username': username})
+    
+    return render(request, 'schedule/home.html', {'role': role, 'username': username})
