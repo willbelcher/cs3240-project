254f118d82b583c86967b610e520ae0877252e6c
Devang Ray <devangray624@gmail.com>
2023-03-14 20:38:18 -0400

    Started on tests

--- login/tests.py
+++ login/tests.py
@@ -1,3 +1,16 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.contrib.auth.models import User
+
 
 # Create your tests here.
+class Test_LoginAuthentication(TestCase):
+    def test_admin_login(self):
+        user = User.objects.create(username='foo', is_superuser=True)
+        factory = RequestFactory()
+
+        request = factory.delete('/')
+        request.user = user
+
+        permission = user.has_perm('global_permissions.is_advisor')
+
+        self.assertTrue(permission)
