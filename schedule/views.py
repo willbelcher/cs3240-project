62fbd6d37b25438525474d26d5b6499b4e96e6c9
Devang Ray <devangray624@gmail.com>
2023-03-14 21:07:16 -0400

    Added a student-only create submission page

--- schedule/views.py
+++ schedule/views.py
@@ -2,31 +2,40 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 
+
 # Create your views here.
 
 @login_required
 def home(request):
-    #Checks to make sure that the user is logged in to assign the correct name
+    # Checks to make sure that the user is logged in to assign the correct name
     if request.user.is_authenticated:
         user = request.user
         role = ""
         username = user.username
-    #Not Logged In is a placeholder currently
+    # Not Logged In is a placeholder currently
     else:
         user = "Not Logged In"
-    
-    #Assigns role (either Student or Advisor) based off of the user permissions
+
+    # Assigns role (either Student or Advisor) based off of the user permissions
     if user.has_perm('global_permissions.is_advisor'):
         role = "Advisor"
     else:
         role = "Student"
-    
+
     return render(request, 'schedule/home.html', {'role': role, 'username': username})
+
 
 def submissions(request):
     user = request.user
     if user.has_perm('global_permissions.is_advisor'):
         return render(request, 'schedule/schedule_submissions.html')
+
+
+def schedules(request):
+    user = request.user
+    if not user.has_perm('global_permissions.is_advisor'):
+        return render(request, 'schedule/schedule_creation.html')
+
 
 def logout_view(request):
     logout(request)
