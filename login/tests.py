20a3c532181e2275ccf78387ef348d72641f88d2
Devang Ray <devangray624@gmail.com>
2023-03-19 15:10:50 -0400

    worked on testing. Different databases is making issues

--- login/tests.py
+++ login/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase, RequestFactory
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group, Permission
+from django.contrib.contenttypes.models import ContentType
 
 
 # Create your tests here.
-class Test_LoginAuthentication(TestCase):
+class TestLoginAuthentication(TestCase):
     def test_admin_login(self):
         user = User.objects.create(username='foo', is_superuser=True)
         factory = RequestFactory()
@@ -14,3 +15,29 @@ class Test_LoginAuthentication(TestCase):
         permission = user.has_perm('global_permissions.is_advisor')
 
         self.assertTrue(permission)
+
+    def test_student_login(self):
+        user = User.objects.create(username='foo')
+        factory = RequestFactory()
+
+        request = factory.delete('/')
+        request.user = user
+
+        permission = user.has_perm('global_permissions.is_advisor')
+
+        self.assertFalse(permission)
+
+    # def test_login_with_user_in_advisor_group(self):
+    #     user = User.objects.create(username='foo')
+    #     factory = RequestFactory()
+    #
+    #     request = factory.delete('/')
+    #     request.user = user
+    #     permission = Permission.objects.create(codename='is_advisor', name="Is Advisor")
+    #
+    #     groups = Group.objects.get_or_create("Advisor")
+    #     groups.permissions.add(permission)
+    #     user.groups.add(groups)
+    #
+    #     permission = user.has_perm('global_permissions.is_advisor')
+    #     self.assertTrue(permission)
--- mysite/settings.py
+++ mysite/settings.py
@@ -12,10 +12,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 LOGIN_REDIRECT_URL = '/schedule'
+LOGOUT_REDIRECT_URL = '/logout'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -26,8 +28,7 @@ SECRET_KEY = 'django-insecure-0nktx!34np*xo)2k^^hzs*at2f3n78#z=ts(6+z3_an_*2i_3j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1','cs3240-b15.herokuapp.com']
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'cs3240-b15.herokuapp.com']
 
 # Application definition
 
@@ -80,31 +81,37 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd3rmtv9e3vskrm',
-        'USER': 'rqdmhmrmzigisx',
-        'PASSWORD': '1e11a105ab659d3df20ae01d4b47448c719f4c00acdfd75572f862dee7498f61',
-        'HOST': 'ec2-54-173-77-184.compute-1.amazonaws.com',
-        'PORT': '5432',
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'BASE_DIR/db.sqlite3'
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'd3rmtv9e3vskrm',
+            'USER': 'rqdmhmrmzigisx',
+            'PASSWORD': '1e11a105ab659d3df20ae01d4b47448c719f4c00acdfd75572f862dee7498f61',
+            'HOST': 'ec2-54-173-77-184.compute-1.amazonaws.com',
+            'PORT': '5432',
+        }
+    }
 
 AUTHENTICATION_BACKENDS = [
-    
+
     # Needed to login by username in Django admin, regardless of `allauth`
     'django.contrib.auth.backends.ModelBackend',
 
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
-    
-]
 
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -123,7 +130,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -134,7 +140,6 @@ TIME_ZONE = 'EST'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
@@ -147,13 +152,13 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 try:
     if 'HEROKU' in os.environ:
         import django_heroku
+
         django_heroku.settings(locals())
 except ImportError:
     found = False
