3dbe368b56be4e91d716b5a8215dc18b72359a88
Devang Ray <devangray624@gmail.com>
2023-03-19 15:36:19 -0400

    fixed some issue with nav bar for Create Schedule (Students) or View Submissions (Advisors)

--- schedule/urls.py
+++ schedule/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('logout/', views.logout_view, name='logout'),
     path('course_search/', views.course_search_view, name='course_search'),
+    path('view_submissions/', views.submissions, name='submissions'),
+    path('create_schedule/', views.schedules, name='schedules'),
 ]
