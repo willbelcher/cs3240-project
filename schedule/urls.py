840241b8ded860a1ddbb9c7094bef6911ed3a86f
glendonchin <glendonchin@gmail.com>
2023-03-27 11:29:39 -0400

    Update urls for cart and add course

--- schedule/urls.py
+++ schedule/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('course_search/', views.course_search_view, name='course_search'),
     path('view_submissions/', views.submissions, name='submissions'),
     path('create_schedule/', views.schedules, name='schedules'),
+    path('add_course/', views.add_course, name='add_course'),
+    path('view_cart/', views.view_cart, name='view_cart'),
 ]
