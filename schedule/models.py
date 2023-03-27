2b7060897f3f243da34a4818eb0d4477528ac531
glendonchin <glendonchin@gmail.com>
2023-03-27 11:29:08 -0400

    Add course and cart models

--- schedule/models.py
+++ schedule/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class ClassSearch(models.Model):
@@ -16,3 +17,15 @@ class ClassSearch(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Cart(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class Course(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    class_nbr = models.IntegerField()
+    subject = models.CharField(max_length=10)
+    catalog_nbr = models.CharField(max_length=10)
+    instructor_name = models.CharField(max_length=50)
+    title = models.CharField(max_length=200)
