7f37b33779420c075679ec390a6db6e5158304de
glendonchin <glendonchin@gmail.com>
2023-03-27 13:24:43 -0400

    Add urls for removing courses and adding course

--- schedule/urls.py
+++ schedule/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('view_submissions/', views.submissions, name='submissions'),
     path('create_schedule/', views.schedules, name='schedules'),
     path('add_course/', views.add_course, name='add_course'),
+    path('add_course_success/', views.add_course, name='add_course_success_url'),
     path('view_cart/', views.view_cart, name='view_cart'),
+    path('remove-course/<int:course_id>/', views.remove_course, name='remove_course'),
 ]
