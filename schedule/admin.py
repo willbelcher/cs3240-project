0abd9a021b2d748955f8269bbacdd2061857055d
Devang Ray <devangray624@gmail.com>
2023-04-04 19:42:55 -0400

    adding all schedule models to admin page

--- schedule/admin.py
+++ schedule/admin.py
@@ -1,4 +1,9 @@
+import django.contrib.auth.apps
 from django.contrib import admin
-from .models import ClassSearch
+from .models import ClassSearch, Cart, Course, Schedule, ScheduleItem
 
 admin.site.register(ClassSearch)
+admin.site.register(Cart)
+admin.site.register(Course)
+admin.site.register(Schedule)
+admin.site.register(ScheduleItem)
