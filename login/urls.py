e6b14d24ae0477be238dcad1fed61f799bf89e66
Devang Ray <devangray624@gmail.com>
2023-04-19 16:05:40 -0400

    resetting these for pr tests

--- login/urls.py
+++ login/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 
 from . import views
 
+app_name = 'schedule'
 urlpatterns = [
     path('', views.login, name='login'),
     path('accounts/', include('allauth.urls'), name='auth'),
