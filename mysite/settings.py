a8b4004048a94a9149c75f1dac648fdc6cf2e24f
Devang Ray <devangray624@gmail.com>
2023-04-30 18:11:48 -0400

    removed "no directory" warning for whitenoise pacakge

--- mysite/settings.py
+++ mysite/settings.py
@@ -177,7 +177,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'schedule/static')
 
 # COMMENT THIS OUT ================================================================
 #SECURE_HSTS_SECONDS = 31536000
